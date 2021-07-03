f495d18729532873270a0db3a34414bdf357b028
stwind <stwindfy@gmail.com>
2021-07-03 12:16:47 +0800

    chore: update AGC001/B

--- agc001/B/main.cpp
+++ agc001/B/main.cpp
@@ -1,25 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
+// clang-format off
+#define forn(i, x, y) for(int i = x; i < y; i++)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+#define MOD 1000000007
+#define INF (1 << 30)
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) (a + b - 1) / b
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template<typename T> void add(T &a, T b) { a += b; if (a >= MOD) a -= MOD; }
+template<typename T> void sub(T &a, T b) { a -= b; if (a < 0) a += MOD; }
+// clang-format on
 
-ll f(ll a, ll b)
-{
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+
+LL calc(LL a, LL b) {
   if (a > b)
-    return f(b, a);
-
-  ll d = b / a;
+    return calc(b, a);
   if (b % a == 0)
-    return (2 * d - 1) * a;
-  else
-    return 2 * d * a + f(b % a, a);
+    return (b / a * 2 - 1) * a;
+
+  return b / a * a * 2 + calc(b % a, a);
 }
 
-int main()
-{
-  ll n, x;
-  cin >> n >> x;
-  ll a = x, b = n - x;
-  cout << a + b + f(a, b) << endl;
+int main() {
+  IOS;
+  LL N, X;
+  cin >> N >> X;
+
+  cout << N + calc(X, N - X) << endl;
+
   return 0;
 }
