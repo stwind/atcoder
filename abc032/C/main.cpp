8f8990a1a9b5fe1c8339911a69bd4ddee0030a5e
stwind <stwindfy@gmail.com>
2021-07-06 10:39:42 +0800

    chore: update ABC032/C

--- abc032/C/main.cpp
+++ abc032/C/main.cpp
@@ -5,40 +5,47 @@ using namespace std;
 #define forn(i, x, y) for(int i = x; i < y; i++)
 #define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
 #define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
 #define MOD 1000000007
+#define INF (1 << 30)
 #define DEBUG(x) cout << #x << ": " << x << endl;
 #define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) (a + b - 1) / b
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template<typename T> void add(T &a, T b) { a += b; if (a >= MOD) a -= MOD; }
+template<typename T> void sub(T &a, T b) { a -= b; if (a < 0) a += MOD; }
 // clang-format on
 
 using LL = long long;
 using VI = vector<int>;
 using VVI = vector<VI>;
 using VLL = vector<LL>;
-using PLL = pair<LL, LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
 
-int main()
-{
-    IOS;
-    int N, K;
-    cin >> N >> K;
-    VI S(N);
-    forn(i, 0, N) cin >> S[i];
+int main() {
+  IOS;
+  int N, K;
+  cin >> N >> K;
+  VI S(N);
+  forn(i, 0, N) cin >> S[i];
 
-    LL cum = 1;
-    int res = 0;
-    for (int i = 0, j = 0; i < N; i++)
-    {
-        if (S[i] == 0)
-        {
-            cout << N << endl;
-            return 0;
-        }
-        cum *= S[i];
-        while (cum > K && j <= i)
-            cum /= S[j++];
-        res = max(res, i - j + 1);
+  LL cur = 1;
+  int res = 0;
+  for (int i = 0, j = 0; j < N; j++) {
+    if (S[j] == 0) {
+      cout << N << endl;
+      return 0;
     }
-    cout << res << endl;
 
-    return 0;
+    cur *= S[j];
+    while (cur > K && i <= j)
+      cur /= S[i++];
+
+    res = max(res, j - i + 1);
+  }
+  cout << res << endl;
+
+  return 0;
 }
