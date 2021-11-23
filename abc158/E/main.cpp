8cf44b15a18b16a54386242248fe9ef01e3e5a49
stwind <stwindfy@gmail.com>
2021-11-23 14:56:01 +0800

    chore: Update ABC158/E

--- abc158/E/main.cpp
+++ abc158/E/main.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
 #define rall(s) s.rbegin(), s.rend()
 #define MOD 1000000007
 #define INF (1 << 30)
 #define DEBUG(x) cout << #x << ": " << x << endl;
 #define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
-#define CEIL(a, b) (a + b - 1) / b
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 template<typename T> void add(T &a, T b) { a += b; if (a >= MOD) a -= MOD; }
@@ -23,40 +25,34 @@ using VVI = vector<VI>;
 using VLL = vector<LL>;
 using VVLL = vector<VLL>;
 using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
 
 int main() {
   IOS;
   int N, P;
-  cin >> N >> P;
   string S;
-  cin >> S;
+  cin >> N >> P >> S;
 
   if (P == 2 || P == 5) {
-    unordered_map<int, int> C;
+    unordered_map<LL, LL> cnt;
     LL res = 0;
-    for (int i = N - 1; i >= 0; i--) {
+    REPR(i, N - 1, 0) {
       int a = S[i] - '0';
-      C[a % P]++;
-      res += C[0];
+      cnt[a % P]++;
+      res += cnt[0];
     }
     cout << res << endl;
     return 0;
   }
 
-  unordered_map<int, int> C;
-  C[0] = 1;
-  LL res = 0;
-  int x = 0, t = 1;
-  for (int i = N - 1; i >= 0; i--) {
-    int a = S[i] - '0';
-
-    x = (t * (a % P) % P + x) % P;
-    t = ((10 % P) * t) % P;
-
-    res += C[x];
-    C[x]++;
+  unordered_map<LL, LL> cnt = {{0, 1}};
+  LL res = 0, s = 0;
+  int base = 1;
+  REPR(i, N - 1, 0) {
+    s = (s + (S[i] - '0') * base % P) % P;
+    res += cnt[s]++;
+    base = base * 10 % P;
   }
-
   cout << res << endl;
 
   return 0;
