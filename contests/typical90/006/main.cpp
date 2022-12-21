d8eacdf7e2cdfe98fc6e3d3ff9b102026cc1796b
stwind <stwindfy@gmail.com>
2022-12-21 09:22:32 +0800

    [typical90] Add 006

--- contests/typical90/006/main.cpp
+++ contests/typical90/006/main.cpp
@@ -1,6 +1,3 @@
-// ref: https://blogger.jinyanakamura.com/2021/04/90-005-restricted-digits7.html
-// ref: https://drken1215.hatenablog.com/entry/2021/10/08/231200
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -29,39 +26,38 @@ using VLL = vector<LL>;
 using VVLL = vector<VLL>;
 using PII = pair<int, int>;
 using PLL = pair<LL, LL>;
-constexpr int LOG = 62;
 
 int main() {
   IOS;
-  LL N;
-  int B, K;
-  cin >> N >> B >> K;
-  VI C(K);
-  REP(i, 0, K) cin >> C[i];
 
-  auto mul = [&](const VLL& dpi, const VLL& dpj, LL tj) {
-    VLL res(B);
-    REP(p, 0, B) REP(q, 0, B) {
-      int nex = (p * tj + q) % B;
-      res[nex] += dpi[p] * dpj[q];
-      res[nex] %= MOD;
+  int N, K;
+  string S;
+  cin >> N >> K >> S;
+
+  int C[26][N + 1];
+  REP(c, 0, 26) {
+    C[c][N] = N;
+    REPR(i, N - 1, -1) {
+      if (S[i] - 'a' == c) C[c][i] = i;
+      else C[c][i] = C[c][i + 1];
     }
-    return res;
-  };
+  }
 
-  VLL ten(LOG);
-  ten[0] = 10;
-  REP(i, 1, LOG) ten[i] = (ten[i - 1] * ten[i - 1]) % B;
-
-  VVLL D(LOG, VLL(B));
-  REP(k, 0, K) D[0][C[k] % B]++;
-  REP(i, 1, LOG) D[i] = mul(D[i - 1], D[i - 1], ten[i - 1]);
-
-  VLL res(B);
-  res[0] = 1;
-  REP(i, 0, LOG) if (N & (1LL << i)) { res = mul(res, D[i], ten[i]); }
-
-  cout << res[0] << endl;
+  string res = "";
+  int i = 0, k = 0, c = 0;
+  while (k < K) {
+    int p = C[c][i];
+    if (N - p >= K - k) {
+      res += c + 'a';
+      k++;
+      i = p + 1;
+      c = 0;
+    }
+    else {
+      c++;
+    }
+  }
+  cout << res << endl;
 
   return 0;
 }
