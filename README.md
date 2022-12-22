e9738db754dcfd9c977ff602bb27f2229de45011
stwind <stwindfy@gmail.com>
2022-12-22 13:01:21 +0800

    [arc032] Add B

--- README.md
+++ README.md
@@ -14,4 +14,5 @@ Playing [AtCoder](https://atcoder.jp/).
 | typical90 | [012 - Red Painting（★4）](https://atcoder.jp/contests/typical90/tasks/typical90_l) | Union Find  |
 | atc001    | [B - Union Find](https://atcoder.jp/contests/atc001/tasks/unionfind_a)              | Union Find  |
 | abc097    | [D - Equals](https://atcoder.jp/contests/abc097/tasks/arc097_b)                     | Union Find  |
+| arc032    | [B - 道路工事](https://atcoder.jp/contests/arc032/tasks/arc032_2)                   | Union Find  |
 | typical90 | [013 - Passing（★5）](https://atcoder.jp/contests/typical90/tasks/typical90_m)      | Dijkstra    |
---
+++ contests/arc032/B/main.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x;  i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+#define MOD 1000000007
+#define INF (1 << 30)
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+// clang-format on
+
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+
+class UnionFind {
+  int n;
+public:
+  VI root, rank, cnt;
+  UnionFind(int n): n(n) {
+    root = rank = VI(n, 0);
+    cnt = VI(n, 1);
+    REP(i, 0, n) root[i] = i;
+  }
+  int find(int x) {
+    return root[x] == x ? x : root[x] = find(root[x]);
+  }
+  int link(int x, int y) {
+    if ((x = find(x)) == (y = find(y))) return x;
+
+    cnt[y] = cnt[x] = cnt[x] + cnt[y];
+
+    if (rank[x] > rank[y]) return root[y] = x;
+    else if (rank[x] < rank[y]) return root[x] = y;
+
+    rank[x] += rank[x] == rank[y];
+    return root[y] = x;
+  }
+  int count(int x) {
+    return cnt[find(x)];
+  }
+  bool query(int x, int y) {
+    return find(x) == find(y);
+  }
+};
+
+int main() {
+  IOS;
+
+  int N, M;
+  cin >> N >> M;
+
+  UnionFind uf = UnionFind(N);
+  int a, b;
+  REP(i, 0, M) {
+    cin >> a >> b;
+    uf.link(--a, --b);
+  }
+
+  unordered_set<int> C;
+  REP(i, 0, N) C.insert(uf.find(i));
+  cout << C.size() - 1 << endl;
+
+  return 0;
+}
