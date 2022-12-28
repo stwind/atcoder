267223b922c6871f7a2d25fb522d2238e4c4b900
stwind <stwindfy@gmail.com>
2022-12-28 09:18:17 +0800

    [chokudai_S001] Add J

--- contests/chokudai_S001/J/main.cpp
+++ contests/chokudai_S001/J/main.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
 #define rall(s) s.rbegin(), s.rend()
 #define MOD 1000000007
-#define INF (1 << 61)
+#define INF (1 << 30)
 #define DEBUG(x) cout << #x << ": " << x << endl;
-#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
 #define CEIL(a, b) ((a) + (b) - 1) / (b)
 #define IN(x, a, b) (a <= x && x < b)
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-template<typename T> void add(T &a, T b) { a += b; if (a >= MOD) a -= MOD; }
-template<typename T> void sub(T &a, T b) { a -= b; if (a < 0) a += MOD; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
 // clang-format on
 
 using LL = long long;
@@ -26,40 +27,51 @@ using VVLL = vector<VLL>;
 using PII = pair<int, int>;
 using PLL = pair<LL, LL>;
 
+template <class T>
+struct BinaryIndexTree {
+  int _n;
+  vector<T> data;
+
+public:
+  BinaryIndexTree(int n): _n(n), data(n, 0) {}
+  void update(int p, T x) {
+    for (; p < _n; p += p & -p)
+      data[p] += x;
+  }
+  T query(int r) {
+    T s = 0;
+    for (; r > 0; r -= r & -r)
+      s += data[r];
+    return s;
+  }
+  int lower_bound(int x) {
+    int s = 0, p = 0;
+    for (int i = log2(_n); i >= 0; i--) {
+      int b = 1 << i;
+      if (p + b < _n && s + data[p + b] < x) {
+        s += data[p += b];
+      }
+    }
+    return p;
+  }
+};
+
 int main() {
   IOS;
+
   int N;
   cin >> N;
   VI A(N);
-  forn(i, 0, N) cin >> A[i];
+  REP(i, 0, N) cin >> A[i];
 
-  VI B(A), C(N);
-  sort(all(B));
-  unordered_map<int, int> V2I;
-  forn(i, 0, N) V2I[B[i]] = i + 1;
-  forn(i, 0, N) C[i] = V2I[A[i]];
-
-  int n = 100001;
-  VI D(n);
-  auto add = [&](int p, int x) {
-    for (; p <= n; p += p & -p) {
-      D[p] += x;
-    }
-  };
-  auto sum = [&](int r) {
-    int s = 0;
-    for (; r > 0; r -= r & -r)
-      s += D[r];
-    return s;
-  };
-
-  LL tot = 0;
-  forn(i, 0, N) {
-    int l = sum(C[i]);
-    tot += C[i] - 1 - l;
-    add(C[i], 1);
+  BinaryIndexTree<int> bit(N + 1);
+  LL res = 0;
+  REP(i, 0, N) {
+    res += A[i] - 1 - bit.query(A[i]);
+    bit.update(A[i], 1);
   }
-  cout << tot << endl;
+
+  cout << res << endl;
 
   return 0;
 }
