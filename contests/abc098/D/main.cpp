5ce3ac2681a4235d773a7e0eed76ea8268852568
stwind <stwindfy@gmail.com>
2022-12-30 17:54:33 +0800

    [abc098] Add D

--- contests/abc098/D/main.cpp
+++ contests/abc098/D/main.cpp
@@ -2,44 +2,55 @@
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
 #define MOD 1000000007
+#define INF (1 << 30)
 #define DEBUG(x) cout << #x << ": " << x << endl;
-#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
 // clang-format on
 
 using LL = long long;
 using VI = vector<int>;
 using VVI = vector<VI>;
 using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
 
-int main()
-{
-    IOS;
-    int N;
-    cin >> N;
-    VI A(N);
-    forn(i, 0, N) cin >> A[i];
+int main() {
+  IOS;
 
-    VI D(20, 0);
+  int N;
+  cin >> N;
+  VI A(N);
+  REP(i, 0, N) cin >> A[i];
 
-    LL res = 0;
-    for (int i = 0, j = 0; i < N; i++)
-    {
-        for (int k = 0; k < 20; k++)
-            D[k] += (A[i] >> k) & 1;
+  VI K(22);
+  auto check = [&]() {
+    REP(i, 0, 22) if (K[i] > 1) return false;
+    return true;
+  };
 
-        while (any_of(all(D), [](int x) { return x >= 2; }) && j < i)
-        {
-            for (int k = 0; k < 20; k++)
-                D[k] -= (A[j] >> k) & 1;
-            j++;
-        }
-        res += i - j + 1;
+  LL res = 0;
+  for (int i = 0, j = 0; i < N;i++) {
+    REP(k, 0, 22) K[k] += (A[i] >> k) & 1;
+    while (!check()) {
+      REP(k, 0, 22) K[k] -= (A[j] >> k) & 1;
+      j++;
     }
-    cout << res << endl;
+    res += i - j + 1;
+  }
+  cout << res << endl;
 
-    return 0;
+  return 0;
 }
