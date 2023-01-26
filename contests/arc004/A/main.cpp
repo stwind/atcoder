a61e372a0086c7cbf147447a980f67b472f1a23c
stwind <stwindfy@gmail.com>
2023-01-26 09:43:11 +0800

    [arc004] Add A

---
+++ contests/arc004/A/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+#define MOD 998244353
+#define INF (1 << 30)
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return (a * b) % MOD; }
+// clang-format on
+
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+
+int main() {
+  IOS;
+
+  int N;
+  cin >> N;
+  vector<double> X(N), Y(N);
+  REP(i, 0, N) cin >> X[i] >> Y[i];
+
+  double res = 0;
+  REP(i, 0, N) REP(j, 0, N) {
+    double dx = X[j] - X[i], dy = Y[j] - Y[i];
+    chmax(res, sqrt(dx * dx + dy * dy));
+  }
+  cout << fixed << setprecision(8) << res << endl;
+
+  return 0;
+}
