cdf2eae97d8a8f6c839b6bb6bcab0d2f4a3e25b6
stwind <stwindfy@gmail.com>
2023-02-28 08:55:32 +0800

    [abc079] Add D

--- README.md
+++ README.md
@@ -174,3 +174,4 @@ Playing [AtCoder](https://atcoder.jp/).
 | abc264            | [C - Matrix Reducing](https://atcoder.jp/contests/abc264/tasks/abc264_c)                             | bit                                     |
 | abc229            | [C - Cheese](https://atcoder.jp/contests/abc229/tasks/abc229_c)                                      | greedy                                  |
 | abc012            | [D - バスと避けられない運命](https://atcoder.jp/contests/abc012/tasks/abc012_4)                    | Floyd-Warshall                          |
+| abc079            | [D - Wall](https://atcoder.jp/contests/abc079/tasks/abc079_d)                                        | Floyd-Warshall                          |
--- contests/abc079/D/main.cpp
+++ contests/abc079/D/main.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
-#define MOD 1000000007
+#define rall(s) s.rbegin(), s.rend()
+#define MOD 998244353
+#define INF (1 << 30)
 #define DEBUG(x) cout << #x << ": " << x << endl;
-#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
-#define CEIL(a, b) (a + b - 1) / b
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-template<typename T> void add(T &a, T b) { a += b; if (a >= MOD) a -= MOD; }
-template<typename T> void sub(T &a, T b) { a -= b; if (a < 0) a += MOD; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return (a * b) % MOD; }
 // clang-format on
 
 using LL = long long;
@@ -23,22 +28,21 @@ using VVLL = vector<VLL>;
 using PII = pair<int, int>;
 using PLL = pair<LL, LL>;
 
-int main()
-{
-    IOS;
-    int H, W;
-    cin >> H >> W;
-    VVI C(10, VI(10, 0));
-    forn(i, 0, 10) forn(j, 0, 10) cin >> C[i][j];
-    VVI A(H, VI(W));
-    forn(i, 0, H) forn(j, 0, W) cin >> A[i][j];
+int main() {
+  IOS;
 
-    forn(k, 0, 10) forn(i, 0, 10) forn(j, 0, 10)
-        chmin(C[i][j], C[i][k] + C[k][j]);
+  int H, W;cin >> H >> W;
+  VVI C(10, VI(10));
+  REP(i, 0, 10) REP(j, 0, 10) cin >> C[i][j];
+  VVI A(H, VI(W));
+  REP(i, 0, H) REP(j, 0, W) cin >> A[i][j];
 
-    int res = 0;
-    forn(i, 0, H) forn(j, 0, W) if (A[i][j] >= 0) res += C[A[i][j]][1];
-    cout << res << endl;
+  REP(k, 0, 10) REP(i, 0, 10) REP(j, 0, 10)
+    chmin(C[i][j], C[i][k] + C[k][j]);
 
-    return 0;
+  LL res = 0;
+  REP(i, 0, H) REP(j, 0, W) if (A[i][j] >= 0) res += C[A[i][j]][1];
+  cout << res << endl;
+
+  return 0;
 }
