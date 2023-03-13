3e6d292ffd75937b7cc49a19ab7e661e9c143a33
stwind <stwindfy@gmail.com>
2023-03-13 13:58:04 +0800

    [abc253] Add D

--- README.md
+++ README.md
@@ -188,3 +188,4 @@ Playing [AtCoder](https://atcoder.jp/).
 | abc131            | [D - Megalomania](https://atcoder.jp/contests/abc131/tasks/abc131_d)                                        | 区間スケジューリング                |
 | arc106            | [C - Solutions](https://atcoder.jp/contests/arc106/tasks/arc106_c)                                          | 区間スケジューリング                |
 | abc003            | [D - AtCoder 社の冬](https://atcoder.jp/contests/abc003/tasks/abc003_4)                                     | 包除原理                                |
+| abc253            | [D - FizzBuzz Sum Hard](https://atcoder.jp/contests/abc253/tasks/abc253_d)                                  | 包除原理                                |
---
+++ contests/abc253/D/main.cpp
@@ -0,0 +1,45 @@
+// https://tutuz.hateblo.jp/entry/2018/08/23/231229
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+#define MOD 1000000007
+#define INF (1 << 30)
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return ((a) * (b)) % MOD; }
+// clang-format on
+
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+
+int main() {
+  IOS;
+
+  LL N, A, B;cin >> N >> A >> B;
+
+  auto f = [](LL n) { return n * (n + 1) / 2; };
+
+  LL l = lcm(A, B);
+  LL res = f(N) - A * f(N / A) - B * f(N / B) + l * f(N / l);
+  cout << res << endl;
+
+  return 0;
+}
