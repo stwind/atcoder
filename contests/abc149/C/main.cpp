5d9196122443c510093095aa1dcf6a945d21a829
stwind <stwindfy@gmail.com>
2023-03-14 12:21:58 +0800

    [abc149] Add C

--- contests/abc149/C/main.cpp
+++ contests/abc149/C/main.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
 #define rall(s) s.rbegin(), s.rend()
 #define MOD 1000000007
 #define INF (1 << 30)
 #define DEBUG(x) cout << #x << ": " << x << endl;
-#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
-#define CEIL(a, b) (a + b - 1) / b
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-template<typename T> void add(T &a, T b) { a += b; if (a >= MOD) a -= MOD; }
-template<typename T> void sub(T &a, T b) { a -= b; if (a < 0) a += MOD; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return ((a) * (b)) % MOD; }
 // clang-format on
 
 using LL = long long;
@@ -23,28 +26,26 @@ using VVI = vector<VI>;
 using VLL = vector<LL>;
 using VVLL = vector<VLL>;
 using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
 
-int main()
-{
-    IOS;
-    int X;
-    cin >> X;
+int main() {
+  IOS;
 
-    int N = 200000;
-    VI P(N + 1, 1);
-    P[0] = P[1] = 0;
-    for (int i = 2; i <= sqrt(N) + 1; i++)
-    {
-        if (P[i])
-            for (int x = i * i; x <= N; x += i)
-                P[x] = 0;
+  int X; cin >> X;
+
+  auto is_prime = [](int x) -> bool {
+    for (int i = 2; i * i <= x;i++)
+      if (x % i == 0) return false;
+    return true;
+  };
+
+  while (true) {
+    if (is_prime(X)) {
+      cout << X << endl;
+      return 0;
     }
+    X++;
+  }
 
-    forn(i, X, N) if (P[i])
-    {
-        cout << i << endl;
-        return 0;
-    }
-
-    return 0;
+  return 0;
 }
