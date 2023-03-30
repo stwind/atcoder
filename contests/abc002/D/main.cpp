e8b7aa760975b3488aba468665dcf6e5885a37cc
stwind <stwindfy@gmail.com>
2023-03-30 08:36:33 +0800

    [abc002] Add D

--- contests/abc002/D/main.cpp
+++ contests/abc002/D/main.cpp
@@ -2,59 +2,49 @@
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
-#define ALL(s) s.begin(), s.end()
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+#define MOD 998244353
 #define DEBUG(x) cout << #x << ": " << x << endl;
-#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
-#define MOD 1000000007
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
 template <typename T> T add(T a, T b) { return (a + b) % MOD; }
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte) \
-  (byte & 0x010 ? '1' : '0'), \
-  (byte & 0x008 ? '1' : '0'), \
-  (byte & 0x004 ? '1' : '0'), \
-  (byte & 0x002 ? '1' : '0'), \
-  (byte & 0x001 ? '1' : '0')
+template <typename T> T mul(T a, T b) { return 1ULL * a * b % MOD; }
 // clang-format on
 
 using LL = long long;
 using VI = vector<int>;
 using VVI = vector<VI>;
 using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
 
-int main()
-{
-    IOS;
-    int N, M;
-    cin >> N >> M;
-    VI G(N, 0);
-    int x, y;
-    forn(i, 0, M)
-    {
-        cin >> x >> y;
-        x--, y--;
-        G[x] |= (1 << x) | (1 << y);
-        G[y] |= (1 << x) | (1 << y);
-    }
+int main() {
+  IOS;
 
-    int res = 1;
-    forn(s, 0, 1 << N)
-    {
-        int t = (1 << N) - 1;
-        forn(i, 0, N)
-        {
-            if (!(s & (1 << i)))
-                continue;
+  int N, M; cin >> N >> M;
+  VI K(N);
+  REP(i, 0, M) {
+    int x, y; cin >> x >> y; x--, y--;
+    K[x] |= (1 << x) | (1 << y);
+    K[y] |= (1 << x) | (1 << y);
+  }
 
-            t &= G[i];
-        }
+  int res = 1;
+  for (int bit = 0, n = 1 << N; bit < n;bit++) {
+    int t = (1 << N) - 1;
+    REP(i, 0, N) if (bit >> i & 1) t &= K[i];
+    if (bit == t) chmax(res, __builtin_popcount(bit));
+  }
+  cout << res << endl;
 
-        if (s == t)
-            res = max(res, __builtin_popcount(s));
-    }
-    cout << res << endl;
-
-    return 0;
+  return 0;
 }
