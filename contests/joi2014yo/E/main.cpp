3d2484d8045d6def239b7799ee2f04c365463591
stwind <stwindfy@gmail.com>
2023-04-10 17:18:37 +0800

    [joi2014yo] Add E

--- contests/joi2014yo/E/main.cpp
+++ contests/joi2014yo/E/main.cpp
@@ -2,17 +2,31 @@
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+#define FOR_EACH(macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
-#define MOD 1000000007
-#define DEBUG(x) cout << #x << ": " << x << endl;
-#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
-#define CEIL(a, b) (a + b - 1) / b
+#define rall(s) s.rbegin(), s.rend()
+#define MOD 998244353
+#define DBG(x) cout << #x << ": " << x << " ";
+#define DEBUG(...) FOR_EACH(DBG, __VA_ARGS__) cout << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-template<typename T> void add(T &a, T b) { a += b; if (a >= MOD) a -= MOD; }
-template<typename T> void sub(T &a, T b) { a -= b; if (a < 0) a += MOD; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return 1ULL * a * b % MOD; }
 // clang-format on
 
 using LL = long long;
@@ -23,70 +37,48 @@ using VVLL = vector<VLL>;
 using PII = pair<int, int>;
 using PLL = pair<LL, LL>;
 
-int main()
-{
-    IOS;
-    int N, K;
-    cin >> N >> K;
-    VI C(N), R(N);
-    forn(i, 0, N) cin >> C[i] >> R[i];
-    int a, b;
-    VVI adj(N);
-    forn(i, 0, K)
-    {
-        cin >> a >> b;
-        a--;
-        b--;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
+int main() {
+  IOS;
 
-    VVI cost(N, VI(N, INT_MAX));
-    forn(i, 0, N)
-    {
-        cost[i][i] = 0;
+  int N, K; cin >> N >> K;
+  VI C(N), R(N);
+  REP(i, 0, N) cin >> C[i] >> R[i];
+  VVI G(N);
+  REP(i, 0, K) {
+    int a, b; cin >> a >> b; a--, b--;
+    G[a].push_back(b);
+    G[b].push_back(a);
+  }
 
-        queue<int> q;
-        q.push(i);
-        unordered_set<int> seen = {i};
-        forn(j, 0, R[i])
-        {
-            int n = q.size();
-            forn(k, 0, n)
-            {
-                int u = q.front();
-                q.pop();
-                for (auto v : adj[u])
-                {
-                    if (seen.count(v))
-                        continue;
-                    seen.insert(v);
-                    q.push(v);
-                    cost[i][v] = C[i];
-                }
-            }
+  VVI G1(N);
+  REP(i, 0, N) {
+    queue<int> q({ i });
+    unordered_set<int> seen = { i };
+    for (int r = 0; r < R[i] && !q.empty(); r++) {
+      int n = q.size();
+      REP(j, 0, n) {
+        int u = q.front(); q.pop();
+        for (auto v : G[u]) if (!seen.count(v)) {
+          seen.insert(v);
+          G1[i].push_back(v);
+          q.push(v);
         }
+      }
     }
+  }
 
-    VI dist(N, INT_MAX);
-    dist[0] = 0;
-    priority_queue<PII, vector<PII>, greater<PII>> pq;
-    pq.push({0, 0});
-    while (!pq.empty())
-    {
-        auto [_, u] = pq.top();
-        pq.pop();
-        forn(v, 0, N) if (cost[u][v] < INT_MAX)
-        {
-            if (dist[v] > dist[u] + cost[u][v])
-            {
-                dist[v] = dist[u] + cost[u][v];
-                pq.push({dist[v], v});
-            }
-        }
-    }
+  VI D(N, 1 << 30);
+  D[0] = 0;
+  priority_queue<PII, vector<PII>, greater<PII>> q;
+  q.push({ 0,0 });
 
-    cout << dist[N - 1] << endl;
+  while (!q.empty()) {
+    auto [d, u] = q.top(); q.pop();
+    for (auto v : G1[u]) if (D[v] > d + C[u])
+      q.push({ D[v] = d + C[u],v });
+  }
 
-    return 0;
+  cout << D[N - 1] << endl;
+
+  return 0;
 }
