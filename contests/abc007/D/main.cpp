855862c770a6482abe68d8fc369ea5292ac4d6f9
stwind <stwindfy@gmail.com>
2023-05-02 09:23:48 +0800

    [abc007] Add D

--- contests/abc007/D/main.cpp
+++ contests/abc007/D/main.cpp
@@ -2,12 +2,31 @@
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+#define FOR_EACH(macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
 #define MOD 1000000007
-#define DEBUG(x) cout << #x << ": " << x << endl;
-#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define DBG(x) cout << #x << ": " << x << " ";
+#define DEBUG(...) FOR_EACH(DBG, __VA_ARGS__) cout << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return 1ULL * a * b % MOD; }
 // clang-format on
 
 using LL = long long;
@@ -15,38 +34,30 @@ using VI = vector<int>;
 using VVI = vector<VI>;
 using VLL = vector<LL>;
 using VVLL = vector<VLL>;
+using PII = pair<int, int>;
 using PLL = pair<LL, LL>;
 
-LL solve(string N)
-{
-    int n = N.length();
-    vector<VVLL> dp(n + 1, VVLL(2, VLL(2, 0)));
+int main() {
+  IOS;
+
+  LL A, B; cin >> A >> B;
+
+  auto solve = [](LL x) -> LL {
+    string S = to_string(x);
+    int n = S.size();
+    vector<VVLL> dp(n + 1, VVLL(2, VLL(2)));
     dp[0][0][0] = 1;
 
-    forn(i, 0, n)
-    {
-        forn(small, 0, 2)
-        {
-            forn(k, 0, 2)
-            {
-                forn(d, 0, (small ? 9 : N[i] - '0') + 1)
-                {
-                    dp[i + 1][small || d < N[i] - '0'][k || d == 4 || d == 9] += dp[i][small][k];
-                }
-            }
-        }
+    REP(i, 0, n) {
+      int D = S[i] - '0';
+      REP(j, 0, 2) REP(k, 0, 2) REP(d, 0, (k ? 10 : D + 1))
+        dp[i + 1][j || d == 4 || d == 9][k || d < D] += dp[i][j][k];
     }
 
-    return dp[n][0][1] + dp[n][1][1];
-}
+    return dp[n][1][0] + dp[n][1][1];
+  };
 
-int main()
-{
-    IOS;
-    LL A, B;
-    cin >> A >> B;
+  cout << solve(B) - solve(A - 1) << endl;
 
-    cout << solve(to_string(B)) - solve(to_string(A - 1)) << endl;
-
-    return 0;
+  return 0;
 }
