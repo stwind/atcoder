145d8e772b4fc8856047f5710e9eb004d5ac9a71
stwind <stwindfy@gmail.com>
2023-05-03 08:28:00 +0800

    [abc041] Add D

--- README.md
+++ README.md
@@ -313,6 +313,7 @@ Playing [AtCoder](https://atcoder.jp/).
 | abc007               | [D - 禁止された数字](https://atcoder.jp/contests/abc007/tasks/abc007_4)                                     | 桁 DP                                   |
 | abc029               | [D - 1](https://atcoder.jp/contests/abc029/tasks/abc029_d)                                                  | 桁 DP                                   |
 | abc036               | [D - 塗り絵](https://atcoder.jp/contests/abc036/tasks/abc036_d)                                             | 木 DP                                   |
+| abc041               | [D - 徒競走](https://atcoder.jp/contests/abc041/tasks/abc041_d)                                             | DP                                      |
 
 ## References
 
--- contests/abc041/D/main.cpp
+++ contests/abc041/D/main.cpp
@@ -1,24 +1,32 @@
-// ref:
-// https://ikatakos.com/pot/programming_algorithm/graph_theory/topological_sort
-
 #include <bits/stdc++.h>
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+#define FOR_EACH(macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
 #define rall(s) s.rbegin(), s.rend()
 #define MOD 1000000007
-#define INF (1 << 61)
-#define DEBUG(x) cout << #x << ": " << x << endl;
-#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define DBG(x) cout << #x << ": " << x << " ";
+#define DEBUG(...) FOR_EACH(DBG, __VA_ARGS__) cout << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
 #define CEIL(a, b) ((a) + (b) - 1) / (b)
 #define IN(x, a, b) (a <= x && x < b)
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-template<typename T> void add(T &a, T b) { a += b; if (a >= MOD) a -= MOD; }
-template<typename T> void sub(T &a, T b) { a -= b; if (a < 0) a += MOD; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return 1ULL * a * b % MOD; }
 // clang-format on
 
 using LL = long long;
@@ -30,28 +38,30 @@ using PII = pair<int, int>;
 using PLL = pair<LL, LL>;
 
 int main() {
-  int N, M;
-  cin >> N >> M;
+  IOS;
 
-  int x, y;
-  VI G(N);
-  forn(i, 0, M) {
-    cin >> x >> y;
-    x--, y--;
-    G[x] |= 1 << y;
+  int N, M; cin >> N >> M;
+  VVI G(N);
+  REP(i, 0, M) {
+    int x, y; cin >> x >> y;
+    G[--x].push_back(--y);
   }
 
-  VLL dp(1 << N);
+  int n = 1 << N;
+  VLL dp(n);
   dp[0] = 1;
-  forn(i, 0, 1 << N) {
-    forn(j, 0, N) {
-      int x = 1 << j;
-      if ((i & x) == 0 && (G[j] & i) == 0) {
-        dp[i | x] += dp[i];
-      }
+
+  REP(s, 0, n) REP(u, 0, N) {
+    if (s >> u & 1) continue;
+
+    bool ok = true;
+    for (auto v : G[u]) if (s >> v & 1) {
+      ok = false;
+      break;
     }
+    if (ok) dp[s | 1 << u] += dp[s];
   }
-  cout << dp[(1 << N) - 1] << endl;
+  cout << dp[n - 1] << endl;
 
   return 0;
 }
