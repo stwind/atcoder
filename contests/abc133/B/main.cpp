ef5b7127770935b4302fdfe49e7f9658bad9360a
stwind <stwindfy@gmail.com>
2023-06-23 06:44:54 +0800

    [abc133] Add B

--- contests/abc133/B/main.cpp
+++ contests/abc133/B/main.cpp
@@ -2,36 +2,56 @@
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+#define FOR_EACH(macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+#define MOD 1000000007
+#define DBG(x) cout << #x << ": " << x << " ";
+#define DEBUG(...) FOR_EACH(DBG, __VA_ARGS__) cout << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return 1ULL * a * b % MOD; }
 // clang-format on
 
 using LL = long long;
 using VI = vector<int>;
 using VVI = vector<VI>;
 using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
 
-int main()
-{
-    int N, D;
-    cin >> N >> D;
-    VVI X(N, VI(D, 0));
-    forn(i, 0, N) forn(j, 0, D) cin >> X[i][j];
+int main() {
+  IOS;
 
-    int res = 0;
-    forn(i, 0, N)
-    {
-        forn(j, i + 1, N)
-        {
-            double par = 0;
-            forn(k, 0, D) par += (X[i][k] - X[j][k]) * (X[i][k] - X[j][k]);
-            par = sqrt(par);
-            if (par == floor(par))
-                res++;
-        }
-    }
+  int N, D; cin >> N >> D;
+  VVI X(N, VI(D));
+  REP(i, 0, N) REP(j, 0, D) cin >> X[i][j];
 
-    cout << res << endl;
+  int n = 0;
+  REP(i, 0, N) REP(j, i + 1, N) {
+    int d = 0;
+    REP(k, 0, D) d += pow(X[i][k] - X[j][k], 2);
+    int dsq = sqrt(d);
+    if (dsq * dsq == d) n++;
+  }
+  cout << n << endl;
 
-    return 0;
+  return 0;
 }
