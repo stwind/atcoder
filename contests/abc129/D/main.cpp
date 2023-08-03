f5a0fd677ad916f2d3033dbd844bb0a803e4780a
stwind <stwindfy@gmail.com>
2023-08-03 20:43:50 +0800

    [abc129] Add D

--- contests/abc129/D/main.cpp
+++ contests/abc129/D/main.cpp
@@ -2,14 +2,23 @@
 using namespace std;
 
 // clang-format off
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+#define FOR_EACH(macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
 #define REP(i, x, y) for(int i = x; i < y; i++)
 #define REPR(i, x, y) for(int i = x; i >= y; i--)
 #define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
 #define rall(s) s.rbegin(), s.rend()
 #define MOD 1000000007
-#define INF (1 << 30)
-#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DBG(x) cout << #x << ": " << x << " ";
+#define DEBUG(...) FOR_EACH(DBG, __VA_ARGS__) cout << endl;
 #define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
 #define CEIL(a, b) ((a) + (b) - 1) / (b)
 #define IN(x, a, b) (a <= x && x < b)
@@ -17,6 +26,7 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
 template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return 1ULL * a * b % MOD; }
 // clang-format on
 
 using LL = long long;
@@ -29,47 +39,24 @@ using PLL = pair<LL, LL>;
 
 int main() {
   IOS;
-  int H, W;
-  cin >> H >> W;
+
+  int H, W; cin >> H >> W;
   vector<string> S(H);
   REP(i, 0, H) cin >> S[i];
 
-  VVI R(H), C(W);
-  REP(i, 0, H) R[i].push_back(-1);
-  REP(i, 0, W) C[i].push_back(-1);
-
-  REP(i, 0, H) REP(j, 0, W) {
-    if (S[i][j] == '#') {
-      R[i].push_back(j);
-      C[j].push_back(i);
-    }
+  VVI L(H, VI(W)), R(H, VI(W)), U(H, VI(W)), D(H, VI(W));
+  REP(i, 0, H) REP(j, 0, W) if (S[i][j] == '.') {
+    L[i][j] = j > 0 ? L[i][j - 1] + 1 : 1;
+    U[i][j] = i > 0 ? U[i - 1][j] + 1 : 1;
   }
-  REP(i, 0, H) R[i].push_back(W);
-  REP(i, 0, W) C[i].push_back(H);
-
-  auto get = [&](VI &a, int x) {
-    int lo = 0, hi = a.size(), mid;
-    while (hi - lo > 1) {
-      mid = (lo + hi) / 2;
-      if (a[mid] < x)
-        lo = mid;
-      else
-        hi = mid;
-    }
-    return a[hi] - a[lo] - 1;
-  };
+  REPR(i, H - 1, 0) REPR(j, W - 1, 0) if (S[i][j] == '.') {
+    R[i][j] = j < W - 1 ? R[i][j + 1] + 1 : 1;
+    D[i][j] = i < H - 1 ? D[i + 1][j] + 1 : 1;
+  }
 
   int res = 0;
-  REP(i, 0, H) {
-    REP(j, 0, W) {
-      if (S[i][j] == '#')
-        continue;
-
-      int r = get(R[i], j);
-      int c = get(C[j], i);
-      chmax(res, r + c - 1);
-    }
-  }
+  REP(i, 0, H) REP(j, 0, W)
+    chmax(res, L[i][j] + R[i][j] + U[i][j] + D[i][j] - 3);
   cout << res << endl;
 
   return 0;
