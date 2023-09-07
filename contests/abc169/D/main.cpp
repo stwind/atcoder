4a9dc337cddfdf5b37ebeff0d2df9043c5d45f27
stwind <stwindfy@gmail.com>
2023-09-08 05:33:38 +0800

    [abc169] Add D

--- contests/abc169/D/main.cpp
+++ contests/abc169/D/main.cpp
@@ -2,19 +2,31 @@
 using namespace std;
 
 // clang-format off
-#define forn(i, x, y) for(int i = x; i < y; i++)
-#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(NULL)
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+#define FOR_EACH(macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
 #define rall(s) s.rbegin(), s.rend()
 #define MOD 1000000007
-#define INF (1 << 30)
-#define DEBUG(x) cout << #x << ": " << x << endl;
-#define DEBUGV(a) for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
-#define CEIL(a, b) (a + b - 1) / b
+#define DBG(x) cout << #x << ": " << x << " ";
+#define DEBUG(...) FOR_EACH(DBG, __VA_ARGS__) cout << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-template<typename T> void add(T &a, T b) { a += b; if (a >= MOD) a -= MOD; }
-template<typename T> void sub(T &a, T b) { a -= b; if (a < 0) a += MOD; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return 1ULL * a * b % MOD; }
 // clang-format on
 
 using LL = long long;
@@ -23,33 +35,27 @@ using VVI = vector<VI>;
 using VLL = vector<LL>;
 using VVLL = vector<VLL>;
 using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
 
-int main()
-{
-    IOS;
-    LL N;
-    cin >> N;
+int main() {
+  IOS;
 
-    int res = 0;
-    for (int i = 2; i <= sqrt(N); i++)
-    {
-        int k = 0;
-        while (N % i == 0)
-        {
-            k++;
-            N /= i;
-        }
+  LL N; cin >> N;
 
-        for (int j = 1; j <= k; j++)
-        {
-            res++;
-            k -= j;
-        }
-    }
-    if (N > 1)
-        res++;
+  map<LL, LL> P;
 
-    cout << res << endl;
+  LL n = N;
+  for (LL i = 2; i <= sqrt(n); i++) {
+    while (n % i == 0) P[i]++, n /= i;
+  }
+  if (n > 1) P[n]++;
 
-    return 0;
+  int res = 0;
+  for (auto& [k, v] : P) {
+    LL x = (sqrt(v * 8 + 1) - 1) / 2;
+    res += x;
+  }
+  cout << res << endl;
+
+  return 0;
 }
