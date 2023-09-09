79723056a4ab9a71712e346b60dcc182c0ce89ab
stwind <stwindfy@gmail.com>
2023-09-10 05:34:39 +0800

    [arc077] Add D

--- contests/arc077/D/main.cpp
+++ contests/arc077/D/main.cpp
@@ -2,14 +2,23 @@
 using namespace std;
 
 // clang-format off
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+#define FOR_EACH(macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
 #define REP(i, x, y) for(int i = x; i < y; i++)
-#define REPR(i, x, y) for(int i = x;  i >= y; i--)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
 #define IOS ios_base::sync_with_stdio(false); cin.tie(0);
 #define all(s) s.begin(), s.end()
 #define rall(s) s.rbegin(), s.rend()
 #define MOD 1000000007
-#define INF (1 << 30)
-#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DBG(x) cout << #x << ": " << x << " ";
+#define DEBUG(...) FOR_EACH(DBG, __VA_ARGS__) cout << endl;
 #define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
 #define CEIL(a, b) ((a) + (b) - 1) / (b)
 #define IN(x, a, b) (a <= x && x < b)
@@ -17,6 +26,7 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
 template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return 1ULL * a * b % MOD; }
 // clang-format on
 
 using LL = long long;
@@ -30,7 +40,7 @@ using PLL = pair<LL, LL>;
 const int MAX = 510000;
 LL fac[MAX], finv[MAX], inv[MAX];
 
-void combInit() {
+void comb_init() {
   fac[0] = fac[1] = 1;
   finv[0] = finv[1] = 1;
   inv[1] = 1;
@@ -49,21 +59,26 @@ LL comb(int n, int k) {
 
 int main() {
   IOS;
-  combInit();
 
-  int N;
-  cin >> N;
+  int N; cin >> N;
+  VI A(N + 1); REP(i, 0, N + 1) cin >> A[i];
 
-  unordered_map<int, int> A;
-  int a, l, r;
+  comb_init();
+
+  VI P(N + 1, -1);
+  int l = -1, r = -1;
   REP(i, 0, N + 1) {
-    cin >> a;
-    if (A.count(a)) l = A[a], r = i;
-    A[a] = i;
+    if (P[A[i]] >= 0) {
+      l = P[A[i]], r = i;
+      break;
+    }
+    else P[A[i]] = i;
   }
 
   REP(k, 1, N + 2) {
-    cout << sub(comb(N + 1, k), comb(l + N - r, k - 1)) << endl;
+    LL a = comb(N + 1, k);
+    LL b = comb(l + N - r, k - 1);
+    cout << sub(a, b) << endl;
   }
 
   return 0;
