6d4315dfa5433756da4af9270b6b78330c0bf20a
stwind <stwindfy@gmail.com>
2023-09-19 06:47:08 +0800

    [dwacon6th-prelims] Add B

--- README.md
+++ README.md
@@ -493,6 +493,7 @@ Playing [AtCoder](https://atcoder.jp/).
 | abc170                         | [E - Smart Infants](https://atcoder.jp/contests/abc170/tasks/abc170_e)                                           | Multiset                                |
 | abc177                         | [F - I hate Shortest Path Problem](https://atcoder.jp/contests/abc177/tasks/abc177_f)                            | Multiset                                |
 | colopl2018-qual                | [C - すぬけそだて――ごはん――](https://atcoder.jp/contests/colopl2018-qual/tasks/colopl2018_qual_c)            | bit 全探索                              |
+| dwacon6th-prelims              | [B - Fusing Slimes](https://atcoder.jp/contests/dwacon6th-prelims/tasks/dwacon6th_prelims_b)                     | 累積和, 逆元                            |
 
 ## References
 
---
+++ contests/dwacon6th-prelims/B/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+#define FOR_EACH(macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+#define MOD 1000000007
+#define DBG(x) cout << #x << ": " << x << " ";
+#define DEBUG(...) FOR_EACH(DBG, __VA_ARGS__) cout << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return 1ULL * a * b % MOD; }
+// clang-format on
+
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+
+int main() {
+  IOS;
+
+  int N; cin >> N;
+  VLL X(N); REP(i, 0, N) cin >> X[i];
+
+  VLL inv(N), fac(N);
+  fac[0] = fac[1] = 1;
+  inv[1] = 1;
+  REP(i, 2, N) {
+    fac[i] = fac[i - 1] * i % MOD;
+    inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+  }
+
+  VLL P(N + 1);
+  REP(i, 0, N) P[i + 1] = add(P[i], inv[i]);
+
+  LL res = 0;
+  REP(i, 0, N - 1) {
+    LL d = X[i + 1] - X[i];
+    LL p = P[i + 2];
+    res = add(res, mul(p, d));
+  }
+  cout << mul(res, fac[N - 1]) << endl;
+
+  return 0;
+}
