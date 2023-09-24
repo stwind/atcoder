c3bce85bce9c5ce1bf3f14f1af8255de434135e0
stwind <stwindfy@gmail.com>
2023-09-25 05:06:15 +0800

    [dp] Add F

--- contests/dp/F/main.cpp
+++ contests/dp/F/main.cpp
@@ -1,38 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-  string s, t;
-  cin >> s >> t;
-  int m = s.size(), n = t.size();
+// clang-format off
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+#define FOR_EACH(macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+#define REP(i, x, y) for(int i = x; i < y; i++)
+#define REPR(i, x, y) for(int i = x; i >= y; i--)
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+#define MOD 1000000007
+#define DBG(x) cout << #x << ": " << x << " ";
+#define DEBUG(...) FOR_EACH(DBG, __VA_ARGS__) cout << endl;
+#define DEBUGV(a) cout << #a << ": "; for(auto it = a.begin() ; it != a.end(); it++) { cout << *it << " "; } cout << endl;
+#define CEIL(a, b) ((a) + (b) - 1) / (b)
+#define IN(x, a, b) (a <= x && x < b)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template <typename T> T sub(T a, T b) { return (a + MOD - b) % MOD; }
+template <typename T> T add(T a, T b) { return (a + b) % MOD; }
+template <typename T> T mul(T a, T b) { return 1ULL * a * b % MOD; }
+// clang-format on
 
-  vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-  for (int i = 0; i < m; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      if (s[i] == t[j])
-        dp[i + 1][j + 1] = dp[i][j] + 1;
-      else
-        dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
-    }
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<LL>;
+using VVLL = vector<VLL>;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+
+
+int main() {
+  IOS;
+
+  string S, T; cin >> S >> T;
+
+  int m = S.length(), n = T.length();
+
+  VVI dp(m + 1, VI(n + 1));
+  REP(i, 0, m) REP(j, 0, n) {
+    if (S[i] == T[j]) dp[i + 1][j + 1] = dp[i][j] + 1;
+    else dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
   }
 
   string res = "";
-  int i = m, j = n;
-  while (i > 0 && j > 0)
-  {
-    if (dp[i][j] == dp[i - 1][j])
-      i--;
-    else if (dp[i][j] == dp[i][j - 1])
-      j--;
-    else
-    {
-      res = s[i - 1] + res;
+  for (int i = m, j = n; i > 0 && j > 0;) {
+    if (S[i - 1] == T[j - 1]) {
+      res = S[i - 1] + res;
       i--, j--;
     }
+    else {
+      if (dp[i][j - 1] > dp[i - 1][j]) j--;
+      else i--;
+    }
   }
+
   cout << res << endl;
 
   return 0;
